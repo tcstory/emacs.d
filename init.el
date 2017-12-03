d3281259612ef986272406d2f46d9866f354f5ce
再见田园犬 <tcstory@163.com>
2017-12-03 17:38:37 +0800

    先去掉了powerline

--- init.el
+++ init.el
@@ -15,8 +15,8 @@
 		company
 		;; --- Better Editor ---
 		smartparens
-		powerline
-		airline-themes
+		;; powerline
+		;; airline-themes
 		;; --- Minor Mode ---
 		exec-path-from-shell
 		;; --- Themes ---
@@ -73,19 +73,5 @@
 (add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
 
 ;;(powerline-default-theme)
-(require 'airline-themes)
-(load-theme 'airline-molokai)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("73a13a70fd111a6cd47f3d4be2260b1e4b717dbf635a9caee6442c949fad41cd" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;(require 'airline-themes)
+;;(load-theme 'airline-molokai)
